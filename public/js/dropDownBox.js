2e54577ab6dd52fb293ab50447c895ee134c4722
masterkai <kai0919@hotmail.com>
2020-03-22 16:41:34 +0800

    add money_transfer.html page for mobile

--- public/js/dropDownBox.js
+++ public/js/dropDownBox.js
@@ -7,9 +7,12 @@ $(function () {
     if (state.clicked) {
       $(this).find('.icon_chevron-down-light').addClass('icon_chevron-up-light')
       $(this).find('.itemContent').css('display','flex')
+      $(this).find('.dropDownBox').addClass('active')
     } else {
       $(this).find('.icon_chevron-down-light').removeClass('icon_chevron-up-light')
       $(this).find('.itemContent').css('display','none')
+      $(this).find('.dropDownBox').removeClass('active')
+
     }
   })
 })
