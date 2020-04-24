a1b185cabdf7db893e8caf45a34d70fc2168cf7a
masterkai <kai0919@hotmail.com>
2020-04-24 17:17:36 +0800

    add fanBingBing eye blinking effect

--- public/js/animation.js
+++ public/js/animation.js
@@ -5,6 +5,7 @@
   const $fanBIngBing = $('.fanBIngBing'),
     $startingPlayMobile = $('.startingPlayMobile'),
     $head = $('.fanBingBing .head'),
+    $closedEye = $('.eye_closed'),
     $body = $('.fanBingBing .body'),
     $elbow = $('.fanBingBing .elbow'),
     $pokerCoin = $('.fanBingBing .pokerCoin')
@@ -21,6 +22,7 @@
       .set($fanBIngBing, {autoAlpha: 0})
       .set($body, {autoAlpha: 1})
       .set($elbow, {autoAlpha: 1})
+      .set($closedEye, {autoAlpha: 0})
       .set($pokerCoin, {autoAlpha: 0, scale: 0.5})
       .set($head, {autoAlpha: 1, onComplete: showContainer})
 
@@ -40,7 +42,7 @@
     const stuffTl = new TimelineMax();
     stuffTl
       .to($fanBIngBing, 2, {scale: 1.2, autoAlpha: 0}, 0.02)
-      .to($head, 2, {transformOrigin: '60px 94px 0'})
+      .to($head, 2, {transformOrigin: '60px 94px 0', onComplete:startBlinking})
       .to($pokerCoin, 10, {scale: 1.2, autoAlpha: 1, transformOrigin: 'center'}, 0.02)
       .to($elbow, 2, {transformOrigin: '9.5px 104px 0', autoAlpha: 1, onComplete: bowElbow})
       .to($body, 2, {autoAlpha: 1, onComplete: bowTheHead}, 0.02);
@@ -54,6 +56,15 @@
         .to($head, 2, {rotation: -2, ease: Power4.easeInOut})
         .to($head, 2, {rotation: 2, ease: Power4.easeInOut})
         .to($head, 2, {rotation: 0, ease: Power4.easeInOut})
+    }
+
+    function startBlinking() {
+      const birdBlinkTl = new TimelineMax({repeat:-1,repeatDelay:2})
+      birdBlinkTl
+        .set($closedEye, {autoAlpha: 1})
+        .set($closedEye, {autoAlpha: 0}, '+=0.2')
+        .set($closedEye, {autoAlpha: 1},'+=1.2')
+        .set($closedEye, {autoAlpha: 0},'+=0.2')
     }
 
     function bowElbow() {
